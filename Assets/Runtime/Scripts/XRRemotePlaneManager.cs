80f41a775b532897bfb452e59932ad0d3963d2c9
Danny Nguyen <67300046+duc55@users.noreply.github.com>
2023-03-21 00:40:31 -0700

    Refactor XrPlaneManager update to Setup function
    XRRemotePlaneVisual: added Setup function to clean up code in XRRemotePlaneManager
    
    Removed AR Default Plane prefab from the Plane Manager on the server since planes are now rendered on Unity client

--- Assets/Runtime/Scripts/XRRemotePlaneManager.cs
+++ Assets/Runtime/Scripts/XRRemotePlaneManager.cs
@@ -2,13 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Collections;
 
 #if UNITY_EDITOR
 namespace XRRemote
 {
-    using UnityEngine.XR.ARFoundation;
-
     public class XRRemotePlaneManager : MonoBehaviour
     {
         public Transform XRRemotePlaneVisualPrefab;
@@ -29,42 +26,12 @@ namespace XRRemote
 
             for (int i = 0; i < planes.Length; i++) {
                 if (i < xrPlaneVisualList.Count) {
-                    xrPlaneVisualList[i].xrPlaneCenterVisual.position = planes[i].center.ToVector3();
-
-                    NativeArray<Vector2> boundary = new NativeArray<Vector2>(planes[i].boundary.Length, Allocator.Temp);
-                    for (int j = 0; j < planes[i].boundary.Length; j++) {
-                        Vector2 localPosition = planes[i].boundary[j].ToVector2();
-                        boundary[j] = new Vector2(localPosition.x, localPosition.y);
-                    }
-
-                    xrPlaneVisualList[i].arDefaultPlaneMeshFilter.gameObject.transform.position = planes[i].pose.position.ToVector3();
-                    xrPlaneVisualList[i].arDefaultPlaneMeshFilter.gameObject.transform.rotation = new Quaternion(planes[i].pose.rotation.x, planes[i].pose.rotation.y, planes[i].pose.rotation.z, planes[i].pose.rotation.w);
-
-
-                    ARPlaneMeshGenerators.GenerateMesh(
-                        xrPlaneVisualList[i].arDefaultPlaneMeshFilter.mesh, 
-                        planes[i].pose,
-                        boundary
-                    );
-
-                    xrPlaneVisualList[i].gameObject.SetActive(true);
+                    xrPlaneVisualList[i].Setup(planes[i]);
                 } else {
                     Transform newVisualTransform = Instantiate(XRRemotePlaneVisualPrefab);
                     XRRemotePlaneVisual xrRemotePlaneVisual = newVisualTransform.GetComponent<XRRemotePlaneVisual>();
 
-                    xrRemotePlaneVisual.xrPlaneCenterVisual.position = planes[i].centerInPlaneSpace.ToVector3();
-
-                    NativeArray<Vector2> boundary = new NativeArray<Vector2>(planes[i].boundary.Length, Allocator.Temp);
-                    for (int j = 0; j < planes[i].boundary.Length; j++) {
-                        Vector2 localPosition = planes[i].boundary[j].ToVector2();
-                        boundary[j] = new Vector2(planes[i].centerInPlaneSpace.x - localPosition.x, planes[i].centerInPlaneSpace.z - localPosition.y);
-                    }
-
-                    ARPlaneMeshGenerators.GenerateMesh(
-                        xrRemotePlaneVisual.arDefaultPlaneMeshFilter.mesh, 
-                        planes[i].pose,
-                        boundary
-                    );
+                    xrRemotePlaneVisual.Setup(planes[i]);
 
                     xrPlaneVisualList.Add(xrRemotePlaneVisual);
                 }
--- Assets/Runtime/Scripts/XRRemotePlaneVisual.cs
+++ Assets/Runtime/Scripts/XRRemotePlaneVisual.cs
@@ -1,9 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using XRRemote;
 
-public class XRRemotePlaneVisual : MonoBehaviour
+#if UNITY_EDITOR
+namespace XRRemote
 {
-    public Transform xrPlaneCenterVisual;
-    public MeshFilter arDefaultPlaneMeshFilter;
+    using UnityEngine.XR.ARFoundation;
+    
+    public class XRRemotePlaneVisual : MonoBehaviour
+    {
+        public Transform xrPlaneCenterVisual;
+        public MeshFilter arDefaultPlaneMeshFilter;
+
+        public void Setup(XRPlane plane)
+        {
+            xrPlaneCenterVisual.position = plane.center.ToVector3();
+
+            NativeArray<Vector2> boundary = new NativeArray<Vector2>(plane.boundary.Length, Allocator.Temp);
+            for (int j = 0; j < plane.boundary.Length; j++) {
+                Vector2 localPosition = plane.boundary[j].ToVector2();
+                boundary[j] = new Vector2(localPosition.x, localPosition.y);
+            }
+
+            Transform arPlaneTransform = arDefaultPlaneMeshFilter.gameObject.transform;
+            arPlaneTransform.position = plane.pose.position.ToVector3();
+            arPlaneTransform.rotation = new Quaternion(
+                plane.pose.rotation.x, 
+                plane.pose.rotation.y, 
+                plane.pose.rotation.z, 
+                plane.pose.rotation.w
+            );
+
+            ARPlaneMeshGenerators.GenerateMesh(
+                arDefaultPlaneMeshFilter.mesh, 
+                plane.pose,
+                boundary
+            );
+
+            gameObject.SetActive(true);
+        }
+    }
 }
+#endif
