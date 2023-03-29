8e8804db0770f79830fb2aa8f4aee5ac5da5ba12
Danny Nguyen <67300046+duc55@users.noreply.github.com>
2023-03-29 16:04:04 -0700

    feat: Added support for touch to interact with UI object in Unity editor
    Currently, only supports OnClick event for Buttons

--- Assets/Runtime/Scripts/XRRemoteInputSystem.cs
+++ Assets/Runtime/Scripts/XRRemoteInputSystem.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 #if UNITY_EDITOR
 namespace XRRemote
@@ -21,8 +23,13 @@ namespace XRRemote
 
         private Vector2 canvasSize = Vector2.one;
 
+        private GraphicRaycaster m_Raycaster;
+        private EventSystem m_EventSystem;
+        private PointerEventData m_PointerEventData;
+
         private void Awake()
         {
+            //Set up singleton
             if (Instance != null) {
                 if (DebugFlags.displayEditorConnectionStats) {
                     Debug.LogError($"There's more than one XRRemoteInputSystem! " + transform + " = " + Instance);
@@ -33,11 +40,30 @@ namespace XRRemote
             
             Instance = this;
 
+            //Fetch the Raycaster from the GameObject (the Canvas)
             Canvas canvas = FindObjectOfType<Canvas>();
 
             if (canvas != null) {
                 canvasSize.x = canvas.GetComponent<RectTransform>().rect.height;
                 canvasSize.y = canvas.GetComponent<RectTransform>().rect.width;
+            }
+
+            if (canvas == null) {
+                if (DebugFlags.displayEditorConnectionStats) {
+                    Debug.LogError($"XRRemoteInputSystem: Unable to find Canvas. Please make sure there is one in the scene.");
+                }
+                return;
+            }
+
+            //Fetch the Raycaster from the GameObject (the Canvas) and Event System from the Scene
+            m_Raycaster = GameObject.FindObjectOfType<GraphicRaycaster>();
+            m_EventSystem = GameObject.FindObjectOfType<EventSystem>();
+
+            if (canvas == null) {
+                if (DebugFlags.displayEditorConnectionStats) {
+                    Debug.LogError($"XRRemoteInputSystem: Unable to find GraphicRaycaster or EventSystem. Please make sure there is one in the scene.");
+                }
+                return;
             }
         }
 
@@ -65,6 +91,33 @@ namespace XRRemote
 
             //Create an input event on the new Input System
             //InputSystem.QueueStateEvent(Touchscreen.current, new TouchState { touchId = 1, phase = UnityEngine.InputSystem.TouchPhase.Began, position = touchPosition });
+
+            //Handle touch hits on UI objects
+            InteractWithUiObjects();
+        }
+
+        private void InteractWithUiObjects()
+        {
+            //Set up the new Pointer Event
+            m_PointerEventData = new PointerEventData(m_EventSystem);
+            //Set the Pointer Event Position to that of the mouse position
+            m_PointerEventData.position = touchPosition;
+
+            //Create a list of Raycast Results
+            List<RaycastResult> results = new List<RaycastResult>();
+
+            //Raycast using the Graphics Raycaster and mouse click position
+            m_Raycaster.Raycast(m_PointerEventData, results);
+
+            //For every result returned, check if we hit a UI element
+            foreach (RaycastResult result in results)
+            {
+                //On button, invoke its onClick event
+                if (result.gameObject.TryGetComponent(out Button button)) {
+                    Debug.Log("Hit " + result.gameObject.name);
+                    button.onClick?.Invoke();
+                }
+            }
         }
     }
 }
